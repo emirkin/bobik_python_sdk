104972de75f7a2c19d9417bace3376060fcc3fde
Felipe Coelho <fcoelho_9@hotmail.com>
2012-07-23 15:08:54 -0300

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from distutils.core import setup
 
+requirements = ['eventlet>=0.9']
+
 setup(
 	name='Bobik Python SDK',
 	version='1.0',
-	packages=['bobik']
+	packages=['bobik'],
+	install_requires=requirements
 )
