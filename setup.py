b5eac4cc3269dd8b7cd1c62dcc3c4191b43dc294
Felipe Coelho <fcoelho_9@hotmail.com>
2012-07-23 19:22:54 -0300

    setup.py with URL and author

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from distutils.core import setup
 requirements = ['eventlet>=0.9']
 
 setup(
-	name='Bobik Python SDK',
+	name='bobik_python_sdk',
 	version='1.0',
 	packages=['bobik'],
+	url='https://github.com/emirkin/bobik_python_sdk',
+	author='Eugene Mirkin',
+	author_email='support@usebobik.com',
 	install_requires=requirements
 )
