4aad1ca584901bec10051d597422a621c62437a2
Felipe Coelho <fcoelho_9@hotmail.com>
2012-07-23 12:25:51 -0300

    logging.Logger support

--- bobik/main.py
+++ bobik/main.py
@@ -4,14 +4,18 @@ import urllib2
 import json
 import eventlet
 eventlet.monkey_patch()
+import logging
+
 
 class Bobik:
     """
     Class used to interface with the Bobik API.
 
-    :param auth_token: the authentication token assigned to your user
-    :param logger: a logger object to redirect the debus messages
-    :param debug: whether to give more information on the command line about the scrape status
+    :param auth_token: The authentication token assigned to your user
+    :param logger: A custom :class:`logging.Logger` object to use for \
+    logging. May be ``None``, in which case output is controlled by the \
+    ``debug`` parameter
+    :param debug: whether to give debug output when a custom logger is not used
     """
 
     BOBIK_URL = 'https://usebobik.com/api/v1/jobs/'
@@ -21,8 +25,10 @@ class Bobik:
         if logger is not None:
             self.logger = logger
         else:
-            self.logger = None
-        self.debug = debug
+            logging.basicConfig()
+            self.logger = logging.getLogger(__name__)
+            if debug:
+                self.logger.setLevel(logging.INFO)
 
     def call_api(self, query, http_method):
         """
@@ -30,7 +36,8 @@ class Bobik:
         Returns the server response as a string.
 
         :param query: the query parameters, submitted as a dict
-        :param http_method: the method to be used for the request, must be ``"GET"`` or ``"POST"``
+        :param http_method: the method to be used for the request, \
+        must be ``"GET"`` or ``"POST"``
         :rtype: string
         """
         query['auth_token'] = self.auth_token
@@ -46,7 +53,7 @@ class Bobik:
         request.add_header('Accept', 'application/json')
         response = urllib2.urlopen(request)
         return response.read()
-            
+
     def scrape(self, query, success_handler, error_handler):
         """
         Submits a new job for Bobik.
@@ -59,37 +66,29 @@ class Bobik:
         """
         response = self.call_api(query, 'POST')
         json_obj = json.loads(response)
-        if json_obj.has_key('errors'):
+        if 'errors' in json_obj:
             return error_handler('error')
 
         self.wait_and_collect_results(json_obj['job'], success_handler)
         return json_obj['job']
 
-    def check_progress(self, response):
+    def __check_progress(self, response):
         json_obj = json.loads(response)
-        if self.debug:
-            self.log_message('INFO', 'Progress - %d%%'% (float(json_obj['progress'])*100))
+        self.logger.info('Progress - %d%%', (float(json_obj['progress']) * 100))
         if float(json_obj['progress']) < 1.0:
-            return False
+            return (False, json_obj)
         else:
-            return True
-
+            return (True, json_obj)
 
     def wait_and_collect_results(self, job_id, handler):
-        if self.debug:
-            self.log_message('INFO', 'Waiting for job %s to complete' % job_id)
+        self.logger.info('Waiting for job %s to complete', job_id)
         json_obj = {}
         json_obj['job'] = job_id
         response = self.call_api(json_obj, 'GET')
-        job_done = self.check_progress(response)
+        job_done,response_json = self.check_progress(response)
         while not job_done:
-            eventlet.sleep(1)
+            eventlet.sleep(float(response_json['estimated_time_left_ms']) / 1000.0)
             response = self.call_api(json_obj, 'GET')
-            job_done = self.check_progress(response)
+            job_done,response_json = self.check_progress(response)
 
         return handler(response)
-
-    def log_message(self, level, message):
-        if self.logger is None:
-            print ('%s : %s')%(level, message)
-
